bdbb9c2e739a111cebc307c4b24f38e8413efaa0
Fernando Gruning <fernando@bejamas.io>
2020-07-15 19:40:14 -0400

    feat(header): set fixed Header
     - Header background slides down when page has scrolled a certain amount

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
+import { throttle } from "lodash"
+import { useState, useEffect, useRef } from "react"
+
 import { useRouter } from "next/router"
 
 import Navbar from "../Navbar"
-import Container from "../Container"
 import Button from "../Button"
 
-import { LogoWrapper } from "./header.styles"
+import { LogoWrapper, HeaderContainer, HeaderBackground } from "./header.styles"
 
 import HamburgerMenuIcon from "../../assets/icons/wt-ic-hamburger-menu.svg"
 import BackIcon from "../../assets/icons/wt-ic-back.svg"
@@ -20,7 +22,26 @@ interface Props {
 const Header = ({ hotelLogo }: Props) => {
   const router = useRouter()
   const isHome = router.pathname === "/"
+  const [hasScrolled, setHasScrolled] = useState(false)
   const [isNavbarOpen, toggleNavbar] = useToggle(false)
+  const headerRef = useRef<boolean>()
+  headerRef.current = hasScrolled
+
+  // Listen to scroll event and toggle show flag when page has scrolled a certain amount
+  useEffect(() => {
+    const handleScroll = () => {
+      const show = global.window.scrollY > 40
+      if (headerRef.current !== show) {
+        setHasScrolled(show)
+      }
+    }
+    // Throttle the event listener callback for better performance
+    // See: https://css-tricks.com/debouncing-throttling-explained-examples/#throttle
+    document.addEventListener("scroll", throttle(handleScroll, 100))
+    return () => {
+      document.removeEventListener("scroll", throttle(handleScroll, 100))
+    }
+  }, [])
 
   const backToHome = () => {
     router.push("/")
@@ -29,7 +50,8 @@ const Header = ({ hotelLogo }: Props) => {
   return (
     <>
       <Navbar isOpen={isNavbarOpen} guestPhoto={hotelLogo} handleNavbarClick={toggleNavbar} />
-      <Container>
+      <HeaderContainer>
+        <HeaderBackground hasScrolled={hasScrolled} />
         <LogoWrapper>
           <Button onClick={isHome ? toggleNavbar : backToHome} bgColor="white" maxWith="40px" height="40px">
             {isHome ? <HamburgerMenuIcon /> : <BackIcon />}
@@ -39,7 +61,7 @@ const Header = ({ hotelLogo }: Props) => {
             <NotificationIcon />
           </Button>
         </LogoWrapper>
-      </Container>
+      </HeaderContainer>
     </>
   )
 }
