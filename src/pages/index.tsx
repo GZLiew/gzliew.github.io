65d2ed58d11cd19d8a22120cdc9be08c069a4123
GeraldM <gerald@bejamas.io>
2020-07-15 19:44:38 -0600

    fix: rename LayoutNavigation to camelCase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,13 +27,13 @@ const SubHeading = styled.h1`
 interface Props {
   hotelConfig?: HotelConfigProps
   homeData?: HomeProps
-  LayoutNavigation?: any
+  layoutNavigation?: any
   preview?: boolean
 }
 
-export default function Index({ hotelConfig, homeData, LayoutNavigation, preview }: Props) {
+export default function Index({ hotelConfig, homeData, layoutNavigation, preview }: Props) {
   return (
-    <Layout navLinks={LayoutNavigation?.navigation} hotelConfig={hotelConfig} preview={preview}>
+    <Layout navLinks={layoutNavigation?.navigation} hotelConfig={hotelConfig} preview={preview}>
       <Head>
         <title>Welcome to {hotelConfig?.content?.hotel_name}</title>
       </Head>
@@ -57,9 +57,9 @@ export default function Index({ hotelConfig, homeData, LayoutNavigation, preview
 export const getStaticProps: GetStaticProps = async ({ preview = null }) => {
   const hotelConfig = (await getHotelConfiguration(preview)) || []
   const homeData = (await getHomeData(preview)) || []
-  const LayoutNavigation = (await getHotelGlobalNavigation(preview)) || []
+  const layoutNavigation = (await getHotelGlobalNavigation(preview)) || []
 
   return {
-    props: { hotelConfig, homeData, LayoutNavigation, preview }
+    props: { hotelConfig, homeData, layoutNavigation, preview }
   }
 }
