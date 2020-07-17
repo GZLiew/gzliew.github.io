8298b47d8eb1a6fdd65fb6343773a67c20dfc94f
Fernando Gruning <fernando@bejamas.io>
2020-07-17 19:11:17 -0400

    feat(hotel-info): query scrolling_icons & categories
    - Add Typescript interface definitions

--- src/lib/types/hotelInfo.ts
+++ src/lib/types/hotelInfo.ts
@@ -7,6 +7,8 @@ interface HotelInfoContent {
   welcome_text: string
   location: IHotelLocation[]
   hotel_rating: IHotelRating[]
+  scrolling_icons: IScrollingIcon[]
+  categories: ICategory[]
 }
 
 interface IHotelInfoField {
@@ -23,5 +25,33 @@ export interface IHotelLocation extends IHotelInfoField {
 export interface IHotelRating extends IHotelInfoField {
   of: string
   star_number: string
-  _uid: string
+}
+
+export interface IScrollingIcon extends IHotelInfoField {
+  icon: IIconAsset
+  name: string
+}
+
+export interface IIconAsset {
+  alt?: string
+  copyright?: any
+  fieldtype?: "asset"
+  filename?: string
+  focus?: any
+  id?: string
+  name: string
+  title?: string
+}
+
+export interface ICategory extends IHotelInfoField {
+  title?: string
+  description?: string
+  subsections?: ICategorySubsection[]
+  _editable?: string
+}
+
+export interface ICategorySubsection extends IHotelInfoField {
+  title?: string
+  description?: string
+  icon: IIconAsset
 }
