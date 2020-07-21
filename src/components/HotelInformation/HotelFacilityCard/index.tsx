a536bad487d44adfc27ac6eeb5547cbe64473f49
GeraldM <gerald@bejamas.io>
2020-07-20 19:24:08 -0600

    feat(scrolling-icons): add SbEditable

--- src/components/HotelInformation/HotelFacilityCard/index.tsx
+++ src/components/HotelInformation/HotelFacilityCard/index.tsx
@@ -1,23 +1,33 @@
+import SbEditable, { SbEditableContent } from "storyblok-react"
+
 import { Card, Icon } from "./hotelFacilityCard.styles"
 
 import ScrollingIcons from "@/assets/icons/ScrollingIcons"
 
 interface Props {
-  icon?: string
-  cardName: string
+  blok?: Card
   remainingNumber?: string | number
+  customName?: string
 }
 
-const HotelFacilityCard: React.FC<Props> = ({ icon, remainingNumber, cardName, ...props }) => {
-  const formattedName = cardName.split(/^([\S]+)/g).map((w, i) => <span key={`${w}-${i}`}>{w}</span>)
+interface Card extends SbEditableContent {
+  icon_name?: string
+  name?: string
+}
+
+const HotelFacilityCard: React.FC<Props> = ({ blok, remainingNumber, customName, ...props }) => {
+  const name = customName || blok?.name
+  const formattedName = name.split(/^([\S]+)/g).map((w, i) => <span key={`${w}-${i}`}>{w}</span>)
 
   return (
-    <Card {...props}>
-      {/* TODO: Should be replace for svg component */}
-      {icon && <Icon src={ScrollingIcons[icon]} alt={cardName} />}
-      {remainingNumber && <h5>{remainingNumber}+</h5>}
-      <h6>{formattedName}</h6>
-    </Card>
+    <SbEditable content={blok}>
+      <Card {...props}>
+        {/* TODO: Should be replace for svg component */}
+        {blok?.icon_name && <Icon src={ScrollingIcons[blok?.icon_name]} alt={blok?.name} />}
+        {remainingNumber && <h5>{remainingNumber}+</h5>}
+        <h6>{formattedName}</h6>
+      </Card>
+    </SbEditable>
   )
 }
 
