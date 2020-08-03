8189e8705bdd7f8326e6dd7cc4ef8e06a6905e17
Fernando Gruning <fernando@bejamas.io>
2020-08-03 15:54:48 -0400

    chore: replace HotelRating icon

--- src/components/HotelInformation/HotelRating/index.tsx
+++ src/components/HotelInformation/HotelRating/index.tsx
@@ -20,6 +20,8 @@ const HotelRating = ({ rating }: Props) => {
       <RatingWrapper>
         <Ratings
           rating={ratedStars}
+          svgIconPaths="M7.9 3.471c.06.131.183.223.325.245l3.408.522c.347.053.49.478.244.73L9.384 7.53a.434.434 0 00-.117.372l.585 3.594a.432.432 0 01-.636.449L6.21 10.278a.431.431 0 00-.418 0l-3.007 1.667a.432.432 0 01-.636-.449l.585-3.594a.434.434 0 00-.117-.372L.123 4.968a.433.433 0 01.244-.73l3.408-.522A.432.432 0 004.1 3.47L5.61.25a.432.432 0 01.782 0L7.9 3.471z"
+          svgIconViewBoxes="0 0 12 12"
           widgetDimensions="12px"
           widgetSpacings="1.5px"
           widgetEmptyColors={theme.colors.ratings.empty}
